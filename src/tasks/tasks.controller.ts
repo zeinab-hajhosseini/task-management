45a7512e039ee6608c517025e216c50a4e40a538
zeinab-hajhosseini <zeinab.hajhosseini72@gmail.com>
2023-01-30 17:06:35 +0330

    Implement createTask in contoller side

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService){}
+    constructor(private tasksService: TasksService) { }
 
     @Get()
-    getAllTasks(): Task[]{
+    getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body('title') title: string, @Body('description') description: string): Task {
+        return this.tasksService.createTask(title, description);
     }
 }
