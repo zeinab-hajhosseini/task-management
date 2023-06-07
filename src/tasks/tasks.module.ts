2890aa0645d74fc254cc4e3befd5a1112818f3a7
zeinab-hajhosseini <zeinab.hajhosseini72@gmail.com>
2023-06-07 15:50:14 +0330

    + Added Gaurd to entire task controller

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { TaskEntity } from './task.entity';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskEntity])],
+  imports: [TypeOrmModule.forFeature([TaskEntity]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
