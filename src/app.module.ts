a60fd4fccdd453ca9fdf67e1a3e5fd31a6bb499d
zeinab-hajhosseini <zeinab.hajhosseini72@gmail.com>
2023-06-11 19:27:14 +0330

    + Implement Config schema for validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: `.env.stage.${process.env.STAGE}` }),
+    ConfigModule.forRoot({
+      envFilePath: `.env.stage.${process.env.STAGE}`,
+      validationSchema: configValidationSchema,
+    }),
     TasksModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
