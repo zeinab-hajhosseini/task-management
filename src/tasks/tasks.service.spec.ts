1e8a262e291e01fb2ea9694a6e0d58fd143759aa
zeinab-hajhosseini <zeinab.hajhosseini72@gmail.com>
2023-06-19 20:03:49 +0330

    + Implement Test for Create Task in service

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,11 +1,29 @@
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserEntity } from 'src/auth/user.entity';
+import { TaskStatus } from './task-status.enum';
 import { TaskEntity } from './task.entity';
 import { TasksService } from './tasks.service';
 
 describe('TasksService', () => {
   let tasksService: TasksService;
-  const mocktasksRepository = {};
+
+  const mockUser: UserEntity = {
+    id: '1',
+    username: 'test user',
+    password: '1',
+    tasks: [],
+  };
+
+  const mocktasksRepository = {
+    create: jest.fn().mockImplementation((dto) => dto),
+    save: jest.fn().mockImplementation((user) =>
+      Promise.resolve({
+        id: Date.now,
+        ...user,
+      }),
+    ),
+  };
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -23,5 +41,22 @@ describe('TasksService', () => {
 
   it('Should be defined', () => {
     expect(tasksService).toBeDefined();
+  });
+
+  it('Should create a new task and return value', async () => {
+    expect(
+      await tasksService.createTask(
+        {
+          title: 'Title',
+          description: 'Description',
+        },
+        mockUser,
+      ),
+    ).toEqual({
+      description: 'Description',
+      status: 'OPEN',
+      title: 'Title',
+      user: mockUser,
+    });
   });
 });
